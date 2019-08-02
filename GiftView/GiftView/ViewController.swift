aaa1456d7b84e56ea74f8f61657b4b3f9ffcf5b7
Art Williamson <alwilliamson@xactware.com>
2019-08-02 13:51:51 -0600

    Project created in Xcode git push
    git status

---
+++ GiftView/GiftView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiftView
+//
+//  Created by Art Williamson on 8/2/19.
+//  Copyright © 2019 Art Williamson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
