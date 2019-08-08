6b5b8614696ee492d0edd560524a93df780046f3
Art Williamson <alwilliamson@xactware.com>
2019-08-08 15:29:59 -0600

    added initial view and buttons

---
+++ GiftView/GiftView/MainSelectionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GiftView
+//
+//  Created by Art Williamson on 8/2/19.
+//  Copyright © 2019 Art Williamson. All rights reserved.
+//
+
+import UIKit
+
+class MainSelectionViewController: UIViewController {
+
+    @IBOutlet weak var titelLabel: UILabel!
+    @IBOutlet weak var birthdayButton: UIButton!
+    @IBOutlet weak var christmasButton: UIButton!
+    @IBOutlet weak var anniversaryButton: UIButton!
+    @IBOutlet weak var otherButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
